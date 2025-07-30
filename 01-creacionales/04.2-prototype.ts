f7617737c9521c789ed8aa1b90578e7d5d19e973
ernesto2011 <skillmx107@gmail.com>
2025-07-29 21:59:59 -0600

    prototype

--- 01-creacionales/04.2-prototype.ts
+++ 01-creacionales/04.2-prototype.ts
@@ -11,19 +11,16 @@
  */
 
 class Pokemon {
-  name: string;
-  type: string;
-  level: number;
-  attacks: string[];
 
-  constructor(name: string, type: string, level: number, attacks: string[]) {
-    throw new Error('Method not implemented.');
+
+  constructor(public name: string, public type: string, public level: number, public attacks: string[]) {
+
+
   }
 
   // Método para clonar el Pokémon
   clone(): Pokemon {
-    // Los ataques deben de evitar pasarse por referencia, es decir, no deben de ser el mismo arreglo.
-    // Completar: Debe devolver un nuevo Pokémon con los mismos atributos
+    return new Pokemon(this.name, this.type, this.level, [...this.attacks])
   }
 
   displayInfo(): void {
@@ -35,6 +32,18 @@ class Pokemon {
   }
 }
 
+
+function main(){
+  const basePokemon = new Pokemon("Charmander", "Fuego", 1, ["Llamarada", "Arañazo"]);
+  const clone1 = basePokemon.clone();
+  clone1.name = "Charmeleon";
+  clone1.level = 16;
+  clone1.attacks.push("Lanzallamas");
+
+  basePokemon.displayInfo(); // Aquí no debe de aparecer "Lanzallamas"
+  clone1.displayInfo();
+}
+main()
 // Tarea:
 // 1. Crear un Pokémon base.
 // 2. Clonar el Pokémon base y modificar algunos atributos en los clones.

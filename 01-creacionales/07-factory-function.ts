9c72d88c62ea24ec882a3d964e530c49f987af15
ernesto2011 <skillmx107@gmail.com>
2025-08-03 11:10:25 -0600

    factory= function

--- 01-creacionales/07-factory-function.ts
+++ 01-creacionales/07-factory-function.ts
@@ -7,3 +7,33 @@
  * * es decir, en tiempo de ejecución y no en tiempo de compilación.
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+type Language = 'es' | 'en' | 'fr'
+
+function createGeeter(lang: Language){
+    return function(name:string){
+        const messages ={
+            es: `Hola, %c${name}`,
+            en: `Hello %c${name}`,
+            fr: `bonjour, %c${name}`
+        }
+
+        return console.log(messages[lang], COLORS.red);
+        
+    }
+}
+
+function main(){
+    const spanishGreeter = createGeeter('es')
+    const englishGreeter = createGeeter('en')
+    const frenchhGreeter = createGeeter('fr')
+
+    spanishGreeter('Ernesto\n')
+    englishGreeter('Ernesto')
+    frenchhGreeter('roots')
+}
+
+
+main()

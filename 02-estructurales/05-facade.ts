7f3f885c2f6ff4530a4359f6bd4aa0f9ac2e0969
ernesto2011 <skillmx107@gmail.com>
2025-08-06 23:26:03 -0600

    :rocket: decorator and facade :rocket:

--- 02-estructurales/05-facade.ts
+++ 02-estructurales/05-facade.ts
@@ -1,3 +1,5 @@
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  * ! Patrón Facade
  * Este patrón proporciona una interfaz unificada para un conjunto de interfaces
@@ -11,3 +13,107 @@
  *
  * https://refactoring.guru/es/design-patterns/facade
  */
+class Projector {
+    on(){
+        console.log('Proyector enccendido');
+    }
+    turnOff(){
+        console.log('Proyecto apagado');
+    }
+}
+
+class SoundSystem{
+    on(){
+        console.log('Sisetem de sonido encendido');
+    }
+    off(){
+        console.log('Sistema de sonido apagado'); 
+    }
+}
+class VideoPlayer {
+    on(){
+        console.log('Video player encendido');
+    }
+    play(movie: string){
+        console.log(`Reproduciendo %c${movie}`, COLORS.blue);
+    }
+    stop(){
+        console.log('Pelicula detenida');
+    }
+    off(){
+        console.log('Video player apagado');
+    }
+}
+
+class PopcornMaker {
+    poppingPopcorn(){
+        console.log('Haciendo palomitas');
+    }
+    turnOffPopcorn(){
+        console.log('Apagando las maquina de palomitas');
+    }
+}
+interface HomeTheaterFacadeOptions{
+    projector: Projector;
+    soundSystem: SoundSystem;
+    videoPlayer: VideoPlayer;
+    popcornMaker: PopcornMaker
+}
+class HomeTheaterFacade {
+    private projector: Projector;
+    private soundSystem: SoundSystem;
+    private videoPlayer: VideoPlayer;
+    private popcornMaker: PopcornMaker;
+
+    constructor({
+        projector,
+        soundSystem,
+        videoPlayer,
+        popcornMaker
+    }:HomeTheaterFacadeOptions){
+        this.projector = projector;
+        this.soundSystem = soundSystem;
+        this.videoPlayer = videoPlayer;
+        this.popcornMaker = popcornMaker
+    }
+    watchMovie(movie: string):void{
+        console.log('%cPreparando para ver las pelicula', COLORS.blue);
+        this.projector.on();
+        this.soundSystem.on()
+        this.popcornMaker.poppingPopcorn()
+        this.videoPlayer.on()
+        this.videoPlayer.play(movie)
+        console.log('%cDisfrute la pelicula', COLORS.green);
+        
+    }
+        endWatchMovie():void{
+        console.log('%cddeteniendo la pelicual', COLORS.blue);
+        this.projector.turnOff()
+        this.soundSystem.off()
+        this.popcornMaker.turnOffPopcorn()
+        this.videoPlayer.stop()
+        this.videoPlayer.off()
+        console.log('%cSistema apagado', COLORS.green);
+        
+    }
+}
+
+async function main(){
+    const projector = new Projector()
+    const soundSystem = new SoundSystem()
+    const videoPlayer = new VideoPlayer()
+    const popcornMaker = new PopcornMaker()
+    
+    const movie = new HomeTheaterFacade({
+        projector,
+        soundSystem,
+        videoPlayer,
+        popcornMaker
+    })
+    movie.watchMovie('Lord of the rings ')
+    
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    movie.endWatchMovie()
+}
+
+main()
--- 02-estructurales/05.2-facade.ts
+++ 02-estructurales/05.2-facade.ts
@@ -58,9 +58,10 @@ class Memory {
 // 2. Clase Facade - ComputerFacade
 
 class ComputerFacade {
-  // TODO: Agregar los atributos necesarios CPU, Memory y HardDrive
+  private cpu: CPU= new CPU();
+  private hardDrive: HardDrive = new HardDrive();
+  private memory: Memory = new Memory();
 
-  // TODO: Agregar el constructor para instanciar los atributos CPU, Memory y HardDrive
   constructor() {}
 
   startComputer(): void {
@@ -68,6 +69,9 @@ class ComputerFacade {
 
     // TODO: ejecutar las operaciones necesarias para encender la computadora
     // 1. Cargar el sistema operativo en la memoria - memory.load(0, hardDrive.read(0, 1024))
+    this.memory.load(0, this.hardDrive.read(0, 1024))
+    this.cpu.jump(0)
+    this.cpu.execute()
     // 2. Saltar a la posición de memoria 0 - cpu.jump(0)
     // 3. Ejecutar las instrucciones del CPU - cpu.execute()
 
@@ -80,6 +84,9 @@ class ComputerFacade {
 
     // TODO: ejecutar las operaciones necesarias para apagar la computadora
     // 1. Detener las operaciones del CPU - cpu.stopOperations()
+    this.cpu.stopOperations()
+    this.memory.free()
+    this.hardDrive.close()
     // 2. Liberar la memoria - memory.free()
     // 3. Cerrar el disco duro - hardDrive.close()
 
@@ -89,12 +96,12 @@ class ComputerFacade {
 
 // 3. Código Cliente para Usar la Facade
 // TODO: Aquí no hay nada que hacer, debe de encender la computadora y apagarla sin problemas
-function main() {
+async function main() {
   const computer = new ComputerFacade();
 
   // Encender la computadora usando la fachada
   computer.startComputer();
-
+  await new Promise((resolve)=>setTimeout(resolve,1000))
   // Apagar la computadora usando la fachada
   computer.shutDownComputer();
 }
